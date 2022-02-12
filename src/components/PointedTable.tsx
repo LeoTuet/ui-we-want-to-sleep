27c74efe309c06c008deeb33b628fc163c4380ca
Luis Q. Haitzer <luis@haitzer.de>
2022-02-12 18:07:54 +0100

    making sheep more responsive and fixing bug

--- src/components/PointedTable.tsx
+++ src/components/PointedTable.tsx
@@ -11,7 +11,10 @@ export const PointedTable = ({ rows }: PointedTableProps) => {
   return (
     <div className={styles.lineTable}>
       {rows.map((row) => (
-        <div className={styles.row}>
+        <div
+          className={styles.row}
+          key={`${row.title}-${row.detail.toString()}`}
+        >
           <p>{row.title}</p>
           <div className={styles.pointDividerContainer}>
             <span className={styles.pointDivider}></span>
