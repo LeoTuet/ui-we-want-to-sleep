21650a62558c3bbea30ccf9349b48d5f940c0f90
94726 <maik.s.kowol@gmail.com>
2022-04-01 23:55:28 +0200

    #67 adds focus trapping to BottomBanner

--- src/components/BottomBanner.tsx
+++ src/components/BottomBanner.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import styles from "./BottomBanner.module.scss";
 import { CustomButton } from "./CustomButton";
 
@@ -12,10 +13,44 @@ export const BottomBanner = ({
   buttonText,
   onButtonClick,
 }: BottomBannerProps) => {
+  const bannerRef = useRef<HTMLDivElement | null>(null);
+
+  const focusHandler = (event: KeyboardEvent) => {    
+    if (event.key == "Tab") {
+      const focusableElements: NodeListOf<HTMLElement> | undefined =
+        bannerRef.current?.querySelectorAll(
+          "a[href], button:not([disabled]), textarea, input, select"
+        );
+
+      if (focusableElements && focusableElements.length) {
+        let first = focusableElements[0];
+        let last = focusableElements[focusableElements.length - 1];
+        let shift = event.shiftKey;
+
+        if (shift && event.target == first) {
+          last.focus();
+          return event.preventDefault();
+        }
+
+        if (!shift && event.target == last) {
+          first.focus();
+          return event.preventDefault();
+        }
+      }
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", focusHandler);
+
+    return () => {
+      window.removeEventListener("keydown", focusHandler);
+    };
+  }, []);
+
   return (
-    <div className={styles.overlay}>
-      <div className={styles.bannerBackground}>
-        <div className={styles.bannerContent}>
+    <div role="presentation" className={styles.overlay}>
+      <div role="dialog" className={styles.bannerBackground}>
+        <div ref={bannerRef} className={styles.bannerContent}>
           <span className={styles.bannerText}>{text}</span>
           <CustomButton onClick={onButtonClick} className={styles.bannerButton}>
             {buttonText}
