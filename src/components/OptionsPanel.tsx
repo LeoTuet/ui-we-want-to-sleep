888c70258d949605ad4dd5ef43ea900ccfd354f3
Ludwig Stecher <ludwig.stecher@gmx.de>
2022-04-03 19:05:18 +0200

    Implement review findings

--- src/components/OptionsPanel.tsx
+++ src/components/OptionsPanel.tsx
@@ -1,14 +1,16 @@
 import styles from "./OptionsPanel.module.scss";
-import { SelectSlider } from "./SelectSlider";
-import halfMoon from "../assets/moon_outline.svg";
+import { Option, SelectSlider } from "./SelectSlider";
+import moon from "../assets/moon_outline.svg";
 import sun from "../assets/sun_outline.svg";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 import { useTheme } from "../hooks/useTheme";
 import { actions } from "../stores/ui";
+import { Theme } from "../models";
 
-export type Language = "de-DE" | "en-US";
-
+/**
+ * Component for selecting the language and theme
+ */
 export function OptionsPanel() {
   const dispatch = useDispatch();
   const { t, i18n } = useTranslation();
@@ -28,32 +30,27 @@ export function OptionsPanel() {
       <SelectSlider
         name="Theme"
         options={[
-          {
-            name: "light",
-            node: (
-              <img
-                src={sun}
-                alt="Sun icon for light theme"
-                aria-label={t("ui.light_theme")}
-                title={t("ui.light_theme")}
-              />
-            ),
-          },
-          {
-            name: "dark",
-            node: (
-              <img
-                src={halfMoon}
-                alt="Moon icon for dark theme"
-                aria-label={t("ui.dark_theme")}
-                title={t("ui.dark_theme")}
-              />
-            ),
-          },
+          option("light", sun, "Sun icon for light theme", t("ui.lightTheme")),
+          option("dark", moon, "Moon icon for dark theme", t("ui.darkTheme")),
         ]}
         value={theme}
         onChange={(theme) => dispatch(actions.setTheme(theme))}
       />
     </div>
   );
+}
+
+/**
+ * Creates an option for a {@link SelectSlider} selecting a theme.
+ */
+function option(
+  name: Theme,
+  src: string,
+  alt: string,
+  label: string
+): Option<Theme> {
+  return {
+    name,
+    node: <img src={src} alt={alt} aria-label={label} title={label} />,
+  };
 }
