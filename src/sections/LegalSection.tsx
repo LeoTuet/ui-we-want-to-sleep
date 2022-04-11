6cd354c1af40c915e45d438bed743ec60402c3a5
Leopold Tüting <leopold@tueting.net>
2022-04-11 20:49:49 +0200

    #60 adds privacy statement translation

--- src/sections/LegalSection.tsx
+++ src/sections/LegalSection.tsx
@@ -3,14 +3,16 @@ import styles from "./LegalSection.module.scss";
 
 interface LegalSectionProps {
   header: string;
+  id?: string;
 }
 
 export const LegalSection = ({
   children,
   header,
+  id = ""
 }: PropsWithChildren<LegalSectionProps>) => {
   return (
-    <section className={styles.legalSection}>
+    <section className={styles.legalSection} id={id}>
       <h2 className={styles.legalSubheader}> {header}</h2>
       <div className={styles.legalText}>{children}</div>
     </section>
