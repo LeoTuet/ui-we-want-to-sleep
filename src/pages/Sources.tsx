3f9107ccab9aab3561c48c0061f4a163ad6e5d2b
Leopold Tueting <leopold@tueting.net>
2022-05-13 12:33:02 +0200

    adds more sources

--- src/pages/Sources.tsx
+++ src/pages/Sources.tsx
@@ -2,21 +2,33 @@ import { t } from "i18next";
 import { ExternalLink } from "../components/ExternalLink";
 import { StickyLegalHeader } from "../components/StickyHeader"
 import { LegalSection } from "../sections/LegalSection";
+import { useTranslation } from "react-i18next";
 import styles from "./Shared.module.scss";
 
 
 export const Sources = () => {
+
+    const { t } = useTranslation();
+
     return(<>
      <StickyLegalHeader text={t("sources.header")} />
      <main className={styles.background}>
         <div className={styles.container}>
-            <LegalSection header="Videos">
+            <LegalSection header={t("sources.videos")}>
                 <p className={styles.list}>
                     <ExternalLink href="https://www.youtube.com/watch?v=1otF0N6surM" />
+                    <ExternalLink href="https://youtu.be/a_UjKdfIj3Q" />
                 </p>
             </LegalSection>
-            <LegalSection header="Lorem ipsum">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <LegalSection header={t("sources.article")}>
+                <p className={styles.list}>
+                    <ExternalLink href="https://www.cdc.gov/sleep/features/schools-start-too-early.html" />
+                    <ExternalLink href="https://www.sleepfoundation.org/school-and-sleep/later-school-start-times" />
+                    <ExternalLink href="https://www.sciencenewsforstudents.org/article/survey-finds-us-schools-start-too-early" />
+                    <ExternalLink href="https://www.educationnext.org/do-schools-begin-too-early/" />
+                    <ExternalLink href="https://www.br.de/nachrichten/wissen/muede-schueler-faengt-die-schule-zu-frueh-an,ReJa68f" />                    
+                    <ExternalLink href="https://www.rtl.de/cms/beginnt-die-schule-morgens-zu-frueh-4416894.html" />                    
+                </p>
             </LegalSection>
         </div>
     </main>
